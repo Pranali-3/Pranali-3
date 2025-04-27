361a6cfb48408b123ab27fc88090ce645928b6d4
Pranali-3 <pranalininawe3@gmail.com>
2025-04-27 16:26:06 +0530

    Update inventory.page.ts

--- pages/inventory.page.ts
+++ pages/inventory.page.ts
@@ -3,10 +3,24 @@ import { Page } from '@playwright/test';
 export class InventoryPage {
   constructor(private page: Page) {}
 
+  async sortBy(option: string) {
+    await this.page.selectOption('.product_sort_container', option);
+  }
+
+  async getAllItemNames() {
+    return this.page.locator('.inventory_item_name').allTextContents();
+  }
+
+  async getAllItemPrices() {
+    const prices = await this.page.locator('.inventory_item_price').allTextContents();
+    return prices.map(price => parseFloat(price.replace('$', '')));
+  }
+
   async addItemToCart(itemName: string) {
-  const item = this.page.locator('.inventory_item').filter({ hasText: itemName });
-  await item.locator('button').click();
-}
+    const item = this.page.locator('.inventory_item').filter({ hasText: itemName });
+    await item.locator('button').click();
+  }
+
   async openCart() {
     await this.page.click('.shopping_cart_link');
   }
